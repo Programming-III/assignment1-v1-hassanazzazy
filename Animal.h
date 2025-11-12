3ef959d25c8812dddeebc00e3470353ed16ec6aa
hassanazzazy <150399701+hassanazzazy@users.noreply.github.com>
2025-11-12 15:21:10 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,17 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
 #include <string>
 using namespace std;
+class Animal{
+    private:
+    string name;
+    int age;
+    bool isHungry;
 
-//define animal class here 
-
-
-
+    public:
+    Animal();
+    Animal(string n ,int a ,bool h);
+    void feed();
+    void display();
+};
 #endif
