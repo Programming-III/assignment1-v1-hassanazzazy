b050f7bd718d6f99a2c8f7398bbac8cf27725666
hassanazzazy <150399701+hassanazzazy@users.noreply.github.com>
2025-11-12 15:37:57 +0200

    Define Visitor class with constructor and display method

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,16 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
 #include <string>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
+class Visitor{
+    private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    Visitor(string n,int t);
+    void displayInfo();
+};
 
 #endif
